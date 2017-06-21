ff0e37d6f1fd850c0ebacdceeddc66c97865ba2c
n-le <n-le@tribalmedia.co.jp>
2017-06-21 20:34:27 +0900

    init getusermedia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+    constraints = {
+        audio: false,
+        video: true
+    };
+
+    ngOnInit(): void {
+        this.getMedia();
+    }
+
+    getMedia = function() {
+        navigator.mediaDevices.getUserMedia(this.constraints).then(this.gotUserMedia).catch(this.didntGetUserMedia);
+    }
+
+    gotUserMedia = function(stream) {
+        var video = document.querySelector('video');
+        video.srcObject = stream;
+        video.onloadedmetadata = function(e) {
+            video.play();
+        };
+    }
+
+    didntGetUserMedia = function(err) {
+        console.log(err.name + ": " + err.message);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar.component';
