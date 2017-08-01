c7d2e150667b8f761501dd347364c41b7259784f
n-le <n-le@tribalmedia.co.jp>
2017-08-01 19:50:39 +0900

    add new dialog

--- src/app/dialog.component.ts
+++ src/app/dialog.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'authencation-failure-dialog',
-  templateUrl: './html/authencation-failure-dialog.html',
+  templateUrl: './html/dialog/authencation-failure-dialog.html',
 })
 
 export class AuthencationFailureDialog {}
+
+@Component({
+  selector: 'already-be-used-dialog',
+  templateUrl: './html/dialog/already-be-used-dialog.html',
+})
+
+export class AlreadyBeUsedDialog {}
