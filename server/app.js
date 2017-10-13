ca26ef37c02104e899ed7438a5ad5f2dd1f41f89
n-le <n-le@tribalmedia.co.jp>
2017-10-13 23:38:28 +0900

    refactor sever code

--- server/app.js
+++ server/app.js
@@ -1,17 +1,14 @@
-var db = require('./database');
+var database = require('./database');
 var core = require('./core');
 var socket = require('socket.io');
+var mysql = require('mysql');
 var Promise = require('promise');
 
-var con = db.con;
 var server = core.server;
 var app = core.app;
 var io = socket(server);
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected database!");
-});
+var options = database.options;
+var connection;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -23,71 +20,103 @@ io.on('connection', function (socket) {
 
     socket.on('auth', function (data) {
 
-        var lid = data.lid;
         return new Promise((resolve, reject) => {
-            var sql = 'SELECT * FROM ?? WHERE ?? = ? AND ?? = ?';
-            con.query(sql, ['users', 'lid', lid, 'del_flg', 0], function (err, result) {
-                if (err) throw err;
-
-                if (result.length == 0) {
-                    socket.emit('auth', {
-                        valid: false
-                    });
-
-                } else if (result[0].login_status != 0 && result[0].socket_id != null) {
-                    socket.emit('auth', {
-                        valid: true,
-                        usable: false
-                    });
-
-                } else {
-                    resolve(result[0]);
-                }
+            connection = mysql.createConnection(options);
+            connection.connect(function(err) {
+                if (err) return reject(err);
+                resolve(data);
             });
+        }).then((data) => {
+            return new Promise((resolve, reject) => {
+                var sql = 'SELECT * FROM ?? WHERE ?? = ? AND ?? = ?';
+                connection.query(sql, ['users', 'lid', data.lid, 'del_flg', 0], function (err, result) {
+                    if (err) return reject(err);
 
-        }).then(function(data) {
-            socket.join(data.room, () => {
-                var sql = 'UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?';
-                con.query(sql, ['users', 'socket_id', socket.id, 'login_status', 1, 'id', data.id], function (err, result) {
-                    if (err) {
-                        return con.rollback(function() {
-                            throw err;
+                    if (result.length == 0) {
+                        socket.emit('auth', {
+                            valid: false
                         });
+                    } else if (result[0].login_status != 0 && result[0].socket_id != null) {
+                        socket.emit('auth', {
+                            valid: true,
+                            usable: false
+                        });
+                    } else {
+                        resolve(result[0]);
                     }
                 });
-            });
-            return Promise.resolve(data);
+            })
+        }).then((data) => {
+            return new Promise((resolve, reject) => {
+                socket.join(data.room, () => {
+                    connection.beginTransaction(function(err) {
+                        if (err) return reject(err);
 
-        }).then(function(data) {
-            var sql = 'SELECT ??, ??, ??, ?? FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ? AND ?? != ?';
-            con.query(sql, ['socket_id', 'username', 'status', 'avatar_url', 'users', 'room', data.room, 'login_status', 1, 'del_flg', 0, 'id', data.id], function (err, result) {
+                        var sql = 'UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?';
+                        connection.query(sql, ['users', 'socket_id', socket.id, 'login_status', 1, 'id', data.id], function (err, result) {
+                            if (err) {
+                                return connection.rollback(function() {
+                                    return reject(err);
+                                });
+                            }
 
-                if (err) throw err;
+                            connection.commit(function(err) {
+                                if (err) {
+                                    return connection.rollback(function() {
+                                        return reject(err);
+                                    });
+                                }
+                                resolve(data);
+                            });
 
-                peers = [];
-                for (var i=0;  i<result.length; i++) {
-                    peers.push({
-                        'sid': result[i].socket_id,
-                        'username': result[i].username,
-                        'status': result[i].status,
-                        'avatar': result[i].avatar_url
+                        });
                     });
-                }
-
-                socket.emit('auth', {
-                    valid: true,
-                    usable: true,
-                    uid: data.id,
-                    username: data.username,
-                    status: data.status,
-                    avatar: data.avatar_url,
-                    room: data.room,
-                    connectable: io.sockets.adapter.rooms[data.room].length > 1 ? true : false,
-                    peers: peers
                 });
-            });
+            })
 
-        }).catch();
+        }).then((data) => {
+            return new Promise((resolve, reject) => {
+                var sql = 'SELECT ??, ??, ??, ?? FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ? AND ?? != ?';
+                connection.query(sql, ['socket_id', 'username', 'status', 'avatar_url', 'users', 'room', data.room, 'login_status', 1, 'del_flg', 0, 'id', data.id], function (err, result) {
+
+                    if (err) return reject(err);
+
+                    peers = [];
+                    for (var i=0;  i<result.length; i++) {
+                        peers.push({
+                            'sid': result[i].socket_id,
+                            'username': result[i].username,
+                            'status': result[i].status,
+                            'avatar': result[i].avatar_url
+                        });
+                    }
+
+                    socket.emit('auth', {
+                        valid: true,
+                        usable: true,
+                        uid: data.id,
+                        username: data.username,
+                        status: data.status,
+                        avatar: data.avatar_url,
+                        room: data.room,
+                        connectable: io.sockets.adapter.rooms[data.room].length > 1 ? true : false,
+                        peers: peers
+                    });
+
+                    resolve();
+                });
+            })
+
+        }).then(() => {
+            return new Promise((resolve, reject) => {
+                connection.end(function(err) {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            })
+        }).catch((err) => {
+            throw err;
+        });
 
     });
 
@@ -96,29 +125,63 @@ io.on('connection', function (socket) {
     });
 
     socket.on('disconnect', (reason) => {
-        var sql = 'SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?';
-        con.query(sql, ['id', 'users', 'socket_id', socket.id, 'login_status', 1], function (err, result) {
-            if (err) throw err;
-
-            if (result.length == 0) return;
-
-            var sql = 'UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?';
-            con.query(sql, ['users', 'socket_id', null, 'login_status', 0, 'socket_id', socket.id], function (err, result) {
-                if (err) {
-                    return con.rollback(function() {
-                        throw err;
-                    });
-                }
+        return new Promise((resolve, reject) => {
+            connection = mysql.createConnection(options);
+            connection.connect(function(err) {
+                if (err) return reject(err);
+                resolve();
             });
+
+        }).then(() => {
+            return new Promise((resolve, reject) => {
+                var sql = 'SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?';
+                connection.query(sql, ['id', 'users', 'socket_id', socket.id, 'login_status', 1], function (err, result) {
+                    if (err) return reject(err);
+
+                    if (result.length == 0) resolve();
+
+                    connection.beginTransaction(function(err) {
+                        if (err) return reject(err);
+
+                        var sql = 'UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?';
+                        connection.query(sql, ['users', 'socket_id', null, 'login_status', 0, 'socket_id', socket.id], function (err, result) {
+                            if (err) {
+                                return connection.rollback(function() {
+                                    return reject(err);
+                                });
+                            }
+
+                            connection.commit(function(err) {
+                                if (err) {
+                                    return connection.rollback(function() {
+                                        return reject(err);
+                                    });
+                                }
+                                resolve();
+                            });
+                        });
+                    });
+                });
+            });
+
+        }).then(() => {
+            return new Promise((resolve, reject) => {
+                connection.end(function(err) {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            })
+        }).catch((err) => {
+            throw err;
         });
     });
 
     socket.on('disconnecting', (reason) => {
-      let rooms = Object.keys(socket.rooms);
-      console.log(rooms);
-      socket.to(rooms[0]).emit('leave', {
-          sid: socket.id
-      });
-  });
+        let rooms = Object.keys(socket.rooms);
+        console.log(rooms);
+        socket.to(rooms[0]).emit('leave', {
+            sid: socket.id
+        });
+    });
 
 });
--- server/database.js
+++ server/database.js
@@ -1,42 +1,40 @@
-var mysql = require('mysql');
-var con;
+var options;
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    con = mysql.createConnection({
+    options = {
       host : "namlh.crhlbrgm4vzq.ap-northeast-1.rds.amazonaws.com",
       user : "lhnam298",
       password : "WvCMBsQdq489h7Hb",
       database : ""
-    });
+    };
     break;
 
   case 'stage':
-    con = mysql.createConnection({
+    options = {
       host : "127.0.0.1",
       user : "root",
       password : "",
       database : "challenge"
-    });
+    };
     break;
 
   case 'development':
-    con = mysql.createConnection({
+    options = {
       host : "challenge-db",
       user : "root",
       password : "",
       database : "challenge"
-    });
+    };
     break;
 
   default:
-    con = mysql.createConnection({
+    options = {
       host : "127.0.0.1",
       user : "root",
       password : "",
       database : "challenge"
-    });
+    };
     break;
 }
-
-exports.con = con;
+exports.options = options;
